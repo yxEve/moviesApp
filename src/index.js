cfbebb0527af6d156bc821d57adae0183526f930
Yuxin Nie <1371454048@qq.com>
2020-10-23 02:28:30 +0100

    Movie Details Page - included production countries

--- src/index.js
+++ src/index.js
@@ -17,60 +17,87 @@ const sample = {
   genres: [
     {
       id: 14,
-      name: "Fantasy"
+      name: "Crime"
     },
     {
       id: 12,
-      name: "Adventure"
+      name: "Thriller"
     },
     {
       id: 878,
-      name: "Science Fiction"
+      name: "Drama"
     },
-    {
-      id: 28,
-      name: "Action"
-    }
+    
   ],
   homepage:
-    "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
+    "https://www.themoviedb.org/movie/475557-joker",
   id: 181808,
   imdb_id: "tt2527336",
   original_language: "en",
   original_title: "Star Wars: The Last Jedi",
   overview:
-    "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+    "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
   popularity: 44.208,
-  poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  poster_path: "/kjNbXFfYLDUdFpfaMkzHbJ5Zq30.jpg",
   production_companies: [
     {
       id: 1,
       logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-      name: "Lucasfilm",
+      name: "DC Entertainment",
       origin_country: "US"
     },
     {
       id: 11092,
       logo_path: null,
-      name: "Ram Bergman Productions",
+      name: "Warner Bros. Pictures",
       origin_country: "US"
     },
     {
       id: 2,
       logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-      name: "Walt Disney Pictures",
+      name: "DC Comics",
       origin_country: "US"
-    }
+    },
+    {
+      id: 3,
+      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+      name: "Joint Effort",
+      origin_country: "US"
+    },
+    {
+      id: 4,
+      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+      name: "Village Roadshow Pictures",
+      origin_country: "US"
+    },
+    {
+      id: 5,
+      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+      name: "JBron Studios",
+      origin_country: "US"
+    },
+    {
+      id: 6,
+      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+      name: "Creative Wealth Media Finance",
+      origin_country: "US"
+    }   
   ],
   production_countries: [
     {
-      iso_3166_1: "US",
-      name: "United States of America"
-    }
+      id: 1,
+      logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
+      name: "Canada",    
+    },
+    {
+        id: 2,
+        logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
+        name: "United States of America",    
+      }
   ],
-  release_date: "2017-12-13",
+  release_date: "2019-10-02",
   revenue: 1332459537,
-  runtime: 152,
+  runtime: 122,
   spoken_languages: [
     {
       iso_639_1: "en",
@@ -79,7 +106,7 @@ const sample = {
   ],
   status: "Released",
   tagline: "Darkness rises... and light to meet it",
-  title: "Star Wars: The Last Jedi",
+  title: "Joker",
   video: false,
   vote_average: 7,
   vote_count: 9692
