43a2311b76ca283d784317c1522183520be5c7f3
Yuxin Nie <1371454048@qq.com>
2020-10-28 15:16:03 +0000

    Added routing and live data from API

--- src/index.js
+++ src/index.js
@@ -1,125 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
 
-const sample = {
-  adult: false,
-  backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
-  belongs_to_collection: {
-    id: 10,
-    name: "Star Wars Collection",
-    poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
-    backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg"
-  },
-  budget: 200000000,
-  genres: [
-    {
-      id: 14,
-      name: "Crime"
-    },
-    {
-      id: 12,
-      name: "Thriller"
-    },
-    {
-      id: 878,
-      name: "Drama"
-    },
-    
-  ],
-  homepage:
-    "https://www.themoviedb.org/movie/475557-joker",
-  id: 181808,
-  imdb_id: "tt2527336",
-  original_language: "en",
-  original_title: "Star Wars: The Last Jedi",
-  overview:
-    "During the 1980s, a failed stand-up comedian is driven insane and turns to a life of crime and chaos in Gotham City while becoming an infamous psychopathic crime figure.",
-  popularity: 44.208,
-  poster_path: "/kjNbXFfYLDUdFpfaMkzHbJ5Zq30.jpg",
-  production_companies: [
-    {
-      id: 1,
-      logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-      name: "DC Entertainment",
-      origin_country: "US"
-    },
-    {
-      id: 11092,
-      logo_path: null,
-      name: "Warner Bros. Pictures",
-      origin_country: "US"
-    },
-    {
-      id: 2,
-      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-      name: "DC Comics",
-      origin_country: "US"
-    },
-    {
-      id: 3,
-      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-      name: "Joint Effort",
-      origin_country: "US"
-    },
-    {
-      id: 4,
-      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-      name: "Village Roadshow Pictures",
-      origin_country: "US"
-    },
-    {
-      id: 5,
-      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-      name: "JBron Studios",
-      origin_country: "US"
-    },
-    {
-      id: 6,
-      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
-      name: "Creative Wealth Media Finance",
-      origin_country: "US"
-    }   
-  ],
-  production_countries: [
-    {
-      id: 1,
-      logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-      name: "Canada",    
-    },
-    {
-        id: 2,
-        logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
-        name: "United States of America",    
-      }
-  ],
-  release_date: "2019-10-02",
-  revenue: 1332459537,
-  runtime: 122,
-  spoken_languages: [
-    {
-      iso_639_1: "en",
-      name: "English"
-    }
-  ],
-  status: "Released",
-  tagline: "Darkness rises... and light to meet it",
-  title: "Joker",
-  video: false,
-  vote_average: 7,
-  vote_count: 9692
-};
-
-const movies = [sample, sample, sample];
-
 const App = () => {
-    return (
+  return (
+    <BrowserRouter>
       <div className="jumbotron">
-        <MoviePage movie={sample} />
+        <div className="container-fluid">
+          <Switch>
+            <Route path="/movies/:id" component={MoviePage} />
+            <Route path="/" component={HomePage} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </div>
       </div>
-    );
-  };
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import Header from "../components/headerMovieList";
 import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
-const MovieListPage = ({movies}) => {
+const MovieListPage = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=61fade314884a3d52cb78924eaeef8c4&language=en-US&include_adult=false&page=1`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json)
+        return json.results
+      })
+      .then(movies => {
+        setMovies(movies);
+      });
+  }, []);
   return (
     <>
       <Header numMovies={movies.length} />
